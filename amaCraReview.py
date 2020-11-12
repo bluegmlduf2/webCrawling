6d70e5d4aa0fabd48beee0d7d386c8f709cc7a01
keung <38097602+bluegmlduf2@users.noreply.github.com>
2020-11-13 02:16:52 +0900

    20201113

--- amaCraReview.py
+++ amaCraReview.py
@@ -1,32 +1,58 @@
 import requests
-from bs4 import BeautifulSoup
-from selenium import webdriver
 import time
+import test
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-#browser = webdriver.Chrome("C:\Users\YOON\Desktop\pythonProj\chromedriver_win32.exe")
-browser = webdriver.Chrome()
+def searchCondition():
+    itemName='マフラー'
+    lang='カタカナ'
+    url="https://www.amazon.co.jp/s?k="+itemName+"&s=review-rank&__mk_ja_JP="+lang+"&ref=sr_pg_1"
+    return url
 
-a='マフラー'
-b='カタカナ'
-#pageCnt=1
+def movePage():
+    try:
+        while True:
+            nextPageTag = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ul.a-pagination>li.a-last')))
+            pageCnt=0
+            #driver.findElements( By.id("...") ).size() != 0
+            #extPageTagChk=driver.findElements(By.CSS_SELECTOR('ul.a-pagination>li.a-last>a')).size() > 0
+            #nextBtnList = driver.find_elements_by_css_selector('ul.a-pagination>li.a-last>a')
+            #nextBtn이disabled 10페이지이상 넘어가면 중단
+            if len(nextPageTag.size)>0 & pageCnt<10:
+                mainNameTag = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'h5.s-line-clamp-1')))
+                priceTag = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.a-price-whole')))
+                #subNameTag = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'span.a-size-base-plus a-color-base a-text-normal')))
+                print('상품명',mainNameTag.text)
+                print('가격',priceTag.text)
+                pageCnt=+1
+                nextPageTag.click()
+            else:
+                break
+    except Exception as ex: # 에러 종류
+        print('에러가 발생 했습니다', ex) # ex는 발생한 에러의 이름
+    finally:
+        driver.quit()
 
-url="https://www.amazon.co.jp/s?k="+a+"&__mk_ja_JP="+b+"&ref=sr_pg_1"
-browser.get(url)
+#Main메서드(스크립트실행:__main__ // import: __모듈명__)
+if __name__ == "__main__":
+    def init():
+        global driver  
+        driver = webdriver.Chrome()#크롬 드라이버(chromedriver_path)
+        driver.get(searchCondition())#조건을 포함한 브라우저를 실행
+
+        global wait
+        wait = WebDriverWait(driver , 10) #10초를 기다린다.
+
+        movePage()#페이지 이동
+
+    # 메인함수실행
+    init()
+    
 
 # liTag = browser.find_elements_by_css_selector('ul.a-pagination>li.a-selected')
-
-while True:
-    nextBtnList = browser.find_elements_by_css_selector('ul.a-pagination>li.a-last>a')
-    pageCnt=0
-    
-    #nextBtn이disabled이거나 10페이지이상 넘어가면 중단
-    if int(len(nextBtnList))>0 & pageCnt<10:
-        pageCnt=+1
-        nextBtnList[0].click()
-        time.sleep(5) 
-    else:
-        break
-
 
 # if int(len(nextBtnList))>0:
 #     nextBtnList[0].click()
