bb062e7c89913d3de3b6dd65c2c82651dde121cd
keung <38097602+bluegmlduf2@users.noreply.github.com>
2020-11-22 09:56:15 +0900

    20201122 번역기능추가

--- amaCraReview.py
+++ amaCraReview.py
@@ -1,22 +1,23 @@
 import requests
 import time
 import test
-#import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from googletrans import Translator
 
 def searchCondition():
     #itemName='マフラー'
     #'パソコン'
     global selectCnt  
     itemName=input('검색어를 입력하세요..')
+    translatedWord=translateJap(itemName)
     selectCnt=int(input('검색할 아이템 수를 입력하세요..'))
 
     lang='カタカナ'
-    url="https://www.amazon.co.jp/s?k="+itemName+"&s=review-rank&__mk_ja_JP="+lang+"&ref=sr_pg_1"
+    url="https://www.amazon.co.jp/s?k="+translatedWord+"&s=review-rank&__mk_ja_JP="+lang+"&ref=sr_pg_1"
     print('검색할 URL...',url)
     return url
 
@@ -30,7 +31,7 @@ def movePage():
             nextPageTag = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'ul.a-pagination>li.a-last')))
             lastPageTag=checkExistElement(driver,'ul.a-pagination>li.a-disabled.a-last')[0]
             
-            #nextBtn이disabled 3페이지이상 넘어가면 중단
+            #nextBtn이disabled ２페이지까지 출력
             if lastPageTag | (pageCnt==2):
                 print('완료되었습니다.')
                 return
@@ -63,6 +64,16 @@ def checkExistElement(item,selector):
     except NoSuchElementException:
         return [False]
     return [True,reVal]
+
+def translateJap(searchWord):
+    try:
+        trans = Translator()
+        time.sleep(0.1)
+        result=trans.translate(searchWord,dest="ja")
+        print('번역한 검색어:',result.text)
+        return result.text
+    except Exception as ex:
+        print('에러가 발생 했습니다', ex) # ex는 발생한 에러의 이름
 
 #Main메서드(스크립트실행:__main__ // import: __모듈명__)
 if __name__ == "__main__":
